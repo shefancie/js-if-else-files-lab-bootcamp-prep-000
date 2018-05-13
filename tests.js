20852833ead4072b6270a51a12cf3755889e20e9
Ayana Singhateh <ayana.singhateh@gmail.com>
2018-05-13 22:22:11 +0000

    automatically backed up by learn

--- tests.js
+++ tests.js
@@ -12,8 +12,4 @@ if (index.height === 70) {
     console.log("The height is correct")
 } else {
     console.log("Expected: 70, Received: "+index.height)
-}if (name === "Susan") {
-    console.log("The name is correct")
-} else {
-    console.log("Expected the name to be Susan")
-}s
+}
