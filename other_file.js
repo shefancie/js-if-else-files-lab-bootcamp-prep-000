ff64e55c571b07a055ab48399fcb180c928622c9
Ayana Singhateh <ayana.singhateh@gmail.com>
2018-05-13 22:24:52 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -6,3 +6,18 @@ var index = require("./index.js")
 
 console.log(index.height)
 
+var index = require("./index.js")
+
+if (index.name === "Susan") {
+    console.log("The name is correct")
+} else {
+    console.log("Expected: Susan, Received: "+index.name)
+}
+
+
+
+if (index.height === 70) {
+    console.log("The height is correct")
+} else {
+    console.log("Expected: 70, Received: "+index.height)
+}
